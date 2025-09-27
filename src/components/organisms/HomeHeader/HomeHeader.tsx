8a1df88a039dcc7eb0f11738beef4ce3db793927
Luiz Fernando <luiz.fpimentel0@gmail.com>
2025-09-27 01:13:52 -0300

    feat(components): add HomeHeader component

---
+++ src/components/organisms/HomeHeader/HomeHeader.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { useTheme } from '@/theme/hooks/useTheme';
+import Icon from 'react-native-vector-icons/Feather';
+
+export default function HomeHeader() {
+  const { layout, spacing, colors } = useTheme();
+
+  return (
+    <View
+      style={[
+        layout.row,
+        layout.itemsCenter,
+        layout.justifyBetween,
+        spacing.py_md,
+        spacing.py_2xl,
+      ]}
+    >
+      <TouchableOpacity>
+        <Icon name="menu" size={28} color={colors.text} />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Icon name="bell" size={24} color={colors.text} />
+      </TouchableOpacity>
+    </View>
+  );
+}
