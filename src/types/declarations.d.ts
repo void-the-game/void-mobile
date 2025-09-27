bb7bec85f36aae527e52539ceb5e9da954112190
Luiz Fernando <luiz.fpimentel0@gmail.com>
2025-09-27 00:15:02 -0300

    chore: declare image types

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,11 @@
+declare module '*.svg' {
+  import * as React from 'react';
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.webp';
