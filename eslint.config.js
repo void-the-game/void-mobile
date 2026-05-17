107f88654c492ef24f5ae5baeb80e23865219eb6
Luiz Fernando <luiz.fpimentel0@gmail.com>
2026-05-17 17:52:23 -0300

    chore: configure eslint rules and git hooks with husky

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,30 @@ const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 const pluginQuery = require('@tanstack/eslint-plugin-query');
+const tseslint = require('typescript-eslint');
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   ...pluginQuery.configs['flat/recommended'],
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', '.expo/*'],
   },
   {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
     rules: {
-      'import/no-unresolved': 'off', // Desabilita essa regra chata
+      'import/no-unresolved': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          tabWidth: 2,
+        },
+      ],
     },
   },
 ]);
